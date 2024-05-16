b9873b1447b977e712f2cdaf7d603c5c1346edb1
Rushi Pisole <rushikeshpisole3@gmail.com>
2024-05-16 17:25:28 +0530

    New Mongo Assignment

---
+++ Mongo/db/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://Rushi:WQm85uIGB4WpNoYB@cluster0.wau9cpo.mongodb.net/Course_selling_app');
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Courses'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
